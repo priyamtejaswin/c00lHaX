930ba30be0c67fef965f5c7c950cb15d42d0f679
Priyam Tejaswin <priyamtejaswin@gmail.com>
2019-07-07 22:17:43 +0530

    Cleanup.
     Changes to be committed:
    	modified:   dtree.py

--- dtree.py
+++ dtree.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 """
-created at: Thu Aug 23 14:35:29 2018
-created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
-
-Implementing the ID3 algorithm.
-
-******************************NOTES******************************
+Priyam Tejaswin (tejaswin.p@flipkart.com)
+Common Decision Tree base class.
 """
+
 
