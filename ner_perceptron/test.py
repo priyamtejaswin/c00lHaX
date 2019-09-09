2319ea09ff3e73338220af1b0eaed5aa6935dc5a
Priyam Tejaswin <priyamtejaswin@gmail.com>
2019-09-10 01:59:44 +0530

    Got the timeit tests to work. 50x speed up yo!
     Changes to be committed:
    	modified:   test.py

--- ner_perceptron/test.py
+++ ner_perceptron/test.py
@@ -6,14 +6,25 @@ created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
 Testing the Cython codes.
 """
 
+import timeit
 import viterbi
 import array
 import numpy as np
+from functools import partial
 from collins_perceptron import decode
 
-word_ixs = array.array('i', range(15))
+word_ixs = array.array('i', range(10))
 num_tags = 500
 weights = np.random.randint(10, size=(50000, num_tags), dtype=int)
 
-print viterbi.decode(word_ixs, num_tags, weights)
-print decode(word_ixs, num_tags, weights)
+#print viterbi.decode(word_ixs, num_tags, weights)
+#print decode(word_ixs, num_tags, weights)
+
+times = timeit.Timer(partial(decode, word_ixs, num_tags, weights)).repeat(3, 100)
+print min(times)/100.0
+times = timeit.Timer(partial(viterbi.decode, word_ixs, num_tags, weights)).repeat(3, 100)
+print min(times)/100.0
+
+
+if __name__ == '__main__':
+    print 'test.py'
