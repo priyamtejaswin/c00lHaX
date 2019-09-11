7d4b5ed390bf7626dc58e1023ad0966c47e87ea2
Priyam Tejaswin <priyamtejaswin@gmail.com>
2019-09-11 19:51:58 +0530

    Added option for profiling the code.
     Changes to be committed:
    	modified:   setup.py

--- ner_perceptron/setup.py
+++ ner_perceptron/setup.py
@@ -10,6 +10,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules=cythonize("viterbi.pyx"),
+    ext_modules=cythonize("viterbi.pyx", annotate=False),
     include_dirs=[numpy.get_include()]
 )
