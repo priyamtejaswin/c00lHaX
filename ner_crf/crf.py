a335e247414e8ddff7a72c55259df76390fe0adb
Priyam Tejaswin <priyamtejaswin@gmail.com>
2019-12-31 16:49:00 +0530

    Initial pass complete. Didn't get much time after the weekend.
     Changes to be committed:
    	modified:   crf.py

--- ner_crf/crf.py
+++ ner_crf/crf.py
@@ -5,5 +5,15 @@ created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
 
 [Lafferty, McCallum, Pereira, "Conditional Random Fields", ICML 2001](https://repository.upenn.edu/cgi/viewcontent.cgi?article=1162&context=cis_papers)
 
+A lot of this code (I/O, utilities, decoding) will be picked from
+`../ner_perceptron/collins_perceptron.py`
+
+### 25th Dec
+[13:35] - Reading the CRF paper again.
+
+[17:09] - Read. Ate. Read some more.
+
+[23:49] - Initial pass through the paper.
+- 
 """
 
