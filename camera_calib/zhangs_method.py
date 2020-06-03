b2569d2308655d8285df5d34c7f39bf54ddd304a
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-06-03 21:20:04 +0530

    Finished plotting code.
     Changes to be committed:
    	modified:   zhangs_method.py

--- camera_calib/zhangs_method.py
+++ camera_calib/zhangs_method.py
@@ -24,7 +24,15 @@ The Zhang paper fits lines to edges of individual squares, and then considers
 the intersection as the corners. A reasonable approach imo. Fish-eye will be
 minimal for small squares.
 
+[9:10 PM] Finished the plotting code.
+FYI --  squares in a file are identified from the bottom-left,
+going left to right. So the first square in the file is the one of the bottom-left,
+the 8th square is on the bottom-right. The last square is on the top-right.
 
+Each corner location in pixel coordinate with the origin at the top-left.
+This follows the image format -- origin for image is at top-left.
+If you read a point as `x, y = point`, on an image, `x` will be the column
+and `y` will be the row.
 """
 
 
@@ -33,4 +41,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def load_points(path):
+    points = []
+    with open(path) as fp:
+        for line in fp.readlines():
+            data = map(float, line.strip().split())
+            square = [(data[i], data[i+1]) for i in range(0, len(data), 2)]
+            points.append(square)
 
+    return points
+
+
+def draw_points(points, image=None, size=(480, 640)):
+    if image is None:
+        final = np.zeros(list(size) + [3])
+    else:
+        if len(image.shape) == 2:
+            # Grayscale ==> RGB
+            final = np.concatenate([np.expand_dims(image, -1)]*3, -1)
+        else:
+            final = np.copy(image)
+
+    for c, square in enumerate(points):
+        for p in square:
+            x, y = p
+            final[int(y), int(x), c%3] = 255
+            final[int(y), int(x), (c+1)%3] = 0
+            final[int(y), int(x), (c+2)%3] = 0
+
+            for i,j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                final[int(y)+i, int(x)+j, c%3] = 255
+                final[int(y)+i, int(x)+j, (c+1)%3] = 0
+                final[int(y)+i, int(x)+j, (c+2)%3] = 0
+
+    plt.imshow(final)
+    plt.show()
+    return final
+
+
+img = cv2.imread('/Users/tejaswin.p/THIS_LAPTOP_projects/c00lHaX/camera_calib/data/zhang_data/CalibIm1.tif', -1)
+points = load_points('/Users/tejaswin.p/THIS_LAPTOP_projects/c00lHaX/camera_calib/data/zhang_data/data1.txt')
+
+print points[0]
+print points[-1]
+
+draw_points([points[0], points[7], points[-1]], img)
