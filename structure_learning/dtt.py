6f85328ea47e4b218acdd53c2664ff8f7521d1f9
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-06-09 14:36:57 +0530

    DTT impementation.
     Changes to be committed:
    	new file:   dtt.py

---
+++ structure_learning/dtt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+created at: Tue Jun  9 14:35:04 2020
+created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
+
+Implements Cheng et. al. solution from 1997:
+"Learning Belief Networks from Data: An Information Theory Approach"
+DTT => Drafting Thickening Thinning
+"""
+
