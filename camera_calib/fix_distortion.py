caf90252c0b29d7331a6a83b828ef0efc2cfa7a0
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-06-14 14:40:45 +0530

    New file for removing distortion.

---
+++ camera_calib/fix_distortion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+created at: Sun Jun 14 14:39:22 2020
+created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
+
+Fix distortion. Pass the image number and image path.
+"""
+
+
