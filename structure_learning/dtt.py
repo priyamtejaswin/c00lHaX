8fe8a76377fd1a1bf1098b24e46a38e2ff437f33
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-06-16 15:19:20 +0530

    Completed. Works. Dumps to D3 force format.
     Changes to be committed:
    	modified:   dtt.py
    	new file:   index.html

--- structure_learning/dtt.py
+++ structure_learning/dtt.py
@@ -11,16 +11,18 @@ DTT => Drafting Thickening Thinning
 
 import os
 import sys
+import json
 import numpy as np
 import networkx as nx
 import cPickle as pickle
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from collections import deque
 from collections import defaultdict
 from itertools import combinations
 
 
-EPSILON = 1e-2
+EPSILON = 0.003
 
 
 def nocond_mi(data, a, b):
@@ -40,15 +42,18 @@ def nocond_mi(data, a, b):
         p_joint[key_joint] += 1
 
     score = 0.0
-    for i in [0, 1]:
-        for j in [0, 1]:
+    uniq_a = set(data[:, a].tolist())
+    uniq_b = set(data[:, b].tolist())
+    
+    for i in uniq_a:
+        for j in uniq_b:
             key = (i, j)
             if key in p_joint:
                 jval = p_joint[key]/float(nsamples)
                 mval_a = p_a[i]/float(nsamples)
                 mval_b = p_b[j]/float(nsamples)
 
-                siter = jval * (np.log2(jval) - (np.log2(mval_a) + np.log2(mval_b)))
+                siter = jval * (np.log(jval) - (np.log(mval_a) + np.log(mval_b)))
                 score += siter
 
     return score
@@ -104,21 +109,29 @@ def cond_mi(data, a, b, condvars):
         key_c = tuple(k[2:])
         p_c = marg_c[key_c]/float(nsamples)
 
-        siter = p_j * (np.log2(p_j) + np.log2(p_c) - np.log2(p_a) - np.log2(p_b))
+        siter = p_j * (np.log(p_j) + np.log(p_c) - np.log(p_a) - np.log(p_b))
         score += siter
 
     return score
 
 
-def get_nbrs_to_sep(G, u, v):
+def get_nbrs_to_sep(G, u, v, parents=None):
     """
-    TODO: Add support to delete nodes from Nbrs_u, Nbrs_v,
+    Done TODO: Add support to delete nodes from Nbrs_u, Nbrs_v,
     if they are KNOWN causal children of u, v.
     """
     Nbrs_u, Nbrs_v = set(), set()
     for path in nx.all_simple_paths(G, u, v):
         Nbrs_u.add(path[1])
         Nbrs_v.add(path[-2])
+
+    if parents is not None:
+        for ix, val in enumerate(parents[:, u].tolist()):
+            if val == 1:
+                Nbrs_u.discard(ix)
+        for ix, val in enumerate(parents[:, v].tolist()):
+            if val == 1:
+                Nbrs_v.discard(ix)
 
     if len(Nbrs_u) > len(Nbrs_v):
         Nbrs_u, Nbrs_v = Nbrs_v, Nbrs_u
@@ -160,13 +173,25 @@ def try_to_separate(u, v, data, Cvars):
                 return False
 
 
-def plot_graph(G):
+def plot_graph(G, parents=None):
     # Plotting code.
     pos = nx.spring_layout(G, iterations=10)
-    nx.draw(G, pos, alpha=0.4, font_size=16, with_labels=True)
+    nx.draw(G, pos, alpha=0.4, font_size=100, with_labels=False)
+    node_labels = {n:v['name'] for n,v in G.nodes(data=True)}
+    nx.draw_networkx_labels(G, pos=pos, labels=node_labels)
     edge_lables = {(e[0], e[1]):round(e[2]['mi_score'], 3) for e in G.edges(data=True)}
     nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_lables)
-    nx.draw_networkx_edges(G, pos=pos)
+    nx.draw_networkx_edges(G, pos=pos, arrows=False)
+
+    if parents is not None:
+        edlist = []
+        for i in range(parents.shape[0]):
+            for j in range(parents.shape[1]):
+                if parents[i, j] == 1:
+                    edlist.append((i, j))
+
+        nx.draw_networkx_edges(G, pos=pos, edgelist=edlist, arrows=True, edge_color='b')
+
     plt.show()
 
 
@@ -179,7 +204,7 @@ def orient_edges(G, data, nvars):
     # Column is PARENT OF Row in `parents` matrix.
     undecided = set()
 
-    for a in G.nodes():
+    for a in tqdm(G.nodes()):
         Sa = list(G.neighbors(a))
         for s1, s2 in combinations(Sa, 2):
             N_a = set()
@@ -190,6 +215,17 @@ def orient_edges(G, data, nvars):
             # Hence, you have to discard.
             N_a.discard(s1)
             N_a.discard(s2)
+
+            if len(N_a) == 0:
+                print "Empty set."
+                raise RuntimeError("For triplet <s1, a, s2>(%d, %d, %d) set N_a is empty!"%(s1, a, s2))
+            if len(N_a) == 1:
+                print "Single element in set.", (s1, a, s2)
+                # undecided.add((s1, a, s2))
+                if cond_mi(data, s1, s2, N_a) > nocond_mi(data, s1, s2)+EPSILON:
+                    parents[a, s1] = 1
+                    parents[a, s2] = 1
+                continue
 
             mi_with_a = cond_mi(data, s1, s2, N_a)
             mi_wout_a = cond_mi(data, s1, s2, N_a-{a})
@@ -209,6 +245,21 @@ def orient_edges(G, data, nvars):
                     if (x, a, p) not in undecided:
                         parents[x, a] = 1
 
+    # Check all triples ...
+    valid_triples = set()
+    for edge in G.edges:
+        u, v = edge
+        for n in G.neighbors(u):
+            if n != v:
+                valid_triples.add((n, u, v))
+        for n in G.neighbors(v):
+            if n != u:
+                valid_triples.add((u, v, n))
+
+    for a,b,c in valid_triples:
+        if parents[b, a] == 1 and parents[b, c] == 0 and parents[c, b] == 0 and (a, b, c) not in undecided:
+            parents[c, b] = 1
+        
     return parents, undecided
         
 
@@ -221,9 +272,10 @@ def main(data, names):
         G.add_node(i, name=names[i])
 
     pairwise_mi_scores = []
-    for a, b in combinations(range(nvars), 2):
+    for a, b in tqdm(combinations(range(nvars), 2)):
         mi_score = nocond_mi(data, a, b)
-        pairwise_mi_scores.append(((a, b), mi_score))
+        if mi_score > EPSILON:
+            pairwise_mi_scores.append(((a, b), mi_score))
 
     L = sorted(pairwise_mi_scores, reverse=True, key=lambda x: x[1])
     for row in L:
@@ -238,7 +290,8 @@ def main(data, names):
 
     nedges = 2
     ptr = 0
-    while L and (nedges < nvars-1):
+    sizeL = len(L)
+    while ptr<sizeL and L and (nedges < nvars-1):
         cedge = L[ptr]
         u, v = cedge[0]
         score = cedge[1]
@@ -249,6 +302,7 @@ def main(data, names):
             G.add_edge(u, v, mi_score=score)
             nedges += 1
             L.pop(ptr)
+            sizeL -= 1
 
     # Drafting complete.
     print "Drafting complete ..."
@@ -278,19 +332,89 @@ def main(data, names):
     plot_graph(G)
 
     # Start orientation.
-    orient_edges(G, data, nvars)
+    parents, undecided = orient_edges(G, data, nvars)
 
+    # Orientation complete.
+    diG = nx.DiGraph()
+    for _n, _d in G.nodes(data=True):
+        diG.add_node(_n, **_d)
+    for etup in G.edges.data():
+        _u, _v, _d = etup
+        diG.add_edge(_u, _v, **_d)
 
+    plot_graph(diG, parents)
 
+    print parents
+
+    # Start thinning.
+    all_edges = list(G.edges.data())
+    for etup in all_edges:
+        u, v, d = etup
+        if len(list(nx.all_simple_paths(G, u, v))) > 1:
+            # Temporarily delete this edge ...
+            G.remove_edge(u, v)
+
+            # Try to separate ...
+            N1, N2 = get_nbrs_to_sep(G, u, v, parents=parents)
+            if len(N1) == 0 and len(N2) == 0:
+                G.add_edge(u, v, **d)
+                continue
+
+            if len(N1)>0 and try_to_separate(u, v, data, N1):
+                pass
+            else:
+                # The first time it fails, re-try with N2.
+                if len(N2)>0 and try_to_separate(u, v, data, N2):
+                    pass
+                else:
+                    G.add_edge(u, v, **d)
+
+    # Thinning complete.
+    plot_graph(G)
+
+    # Export to viz in D3.js
+    nodes, links = [], []
+    for _n, _d in G.nodes(data=True):
+        nodes.append({
+            'name': _d['name'],
+            'label': str(_n),
+            'id': _n
+        })
+    for _u, _v, _d in G.edges(data=True):
+        if parents[_v, _u] == 1:
+            directed = True
+        elif parents[_u, _v] == 1:
+            _v, _u = _u, _v
+            directed = True
+        else:
+            directed = False
+        links.append({
+            'source': _u,
+            'target': _v,
+            'value': _d['mi_score'],
+            'directed': directed
+        })
+    with open('output.json', 'w') as fp:
+        fp.write(json.dumps({'nodes':nodes, 'links':links}, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    # path = sys.argv[1]
-    path = 'data_sample_general.cpkl'
-    assert os.path.exists(path)
+    # # path = sys.argv[1]
+    # path = 'data_sample_general.cpkl'
+    # assert os.path.exists(path)
 
-    with open(path) as fp:
-        data = pickle.load(fp)
-        print data.keys()
+    # with open(path) as fp:
+    #     data = pickle.load(fp)
+    #     print data.keys()
 
-    main(data['data'], data['names'])
+    # main(data['data'], data['names'])
+
+    import pandas as pd
+    from sklearn.preprocessing import LabelEncoder
+    lbe = LabelEncoder()
+    raw = pd.read_csv('~/Downloads/ALARM10k.csv', index_col=None)
+    encoded = {c:lbe.fit_transform(raw[c]) for c in raw.columns}
+    alarm = pd.DataFrame.from_dict(encoded)
+
+    print alarm.shape
+    main(alarm.to_numpy(), alarm.columns)
